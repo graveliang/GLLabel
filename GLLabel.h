ff9231df37d6f56b23054b5e09f24bf6fb94c36c
graveliang <qianfeng@mgk.local>
2014-12-15 21:10:10 +0800

    GLLabel
    价格减价等信息添加中横线

---
+++ GLLabel.h
@@ -0,0 +1,14 @@
+//
+//  GLLabel.h
+//  FreeLimit1409
+//
+//  Created by mac on 14-11-27.
+//  Copyright (c) 2014年 zhang jian. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface GLLabel : UILabel
+//添加中划线
+-(void)setStrikeLine;
+@end
